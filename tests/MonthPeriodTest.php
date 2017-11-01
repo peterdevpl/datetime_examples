c54c278390c76b6b5a92a44965393ab3b41ea1e7
Piotr Horzycki <piotr.horzycki@gmail.com>
2017-11-01 15:07:30 +0100

    Leap year test for MonthPeriod

--- tests/MonthPeriodTest.php
+++ tests/MonthPeriodTest.php
@@ -7,14 +7,25 @@ use Piotr\DateTimeExamples\MonthPeriod;
 
 class MonthPeriodTest extends TestCase
 {
-    public function testMonth()
+    /**
+     * @dataProvider getDateRanges
+     */
+    public function testMonth(string $today, string $expectedStartDay, string $expectedEndDay)
     {
-        $now = new \DateTimeImmutable('2017-11-11');
+        $now = new \DateTimeImmutable($today);
         $period = new MonthPeriod($now);
 
-        $expectedStart = new \DateTimeImmutable('2017-11-01 00:00:00');
-        $expectedEnd = new \DateTimeImmutable('2017-11-30 23:59:59');
+        $expectedStart = new \DateTimeImmutable($expectedStartDay);
+        $expectedEnd = new \DateTimeImmutable($expectedEndDay);
         $this->assertEquals($expectedStart, $period->getStart());
         $this->assertEquals($expectedEnd, $period->getEnd());
+    }
+
+    public function getDateRanges()
+    {
+        return [
+            'November'  => ['2017-11-11', '2017-11-01 00:00:00', '2017-11-30 23:59:59'],
+            'Leap year' => ['2016-02-11', '2016-02-01 00:00:00', '2016-02-29 23:59:59'],
+        ];
     }
 }
